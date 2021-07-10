604d948b0982f13405a772056c17b54db57cf9b0
Noor Alhomsi <nalhomsi@gmail.com>
2021-07-10 04:30:01 -0400

    card colors

---
+++ test/Intern.test.js
@@ -0,0 +1,19 @@
+const Intern = require("../lib/Intern");
+
+test("Can set school via constructor", () => {
+  const testValue = "UT Austin";
+  const e = new Intern("Brian", 1, "test@test.com", testValue);
+  expect(e.school).toBe(testValue);
+});
+
+test("getRole() should return \"Intern\"", () => {
+  const testValue = "Intern";
+  const e = new Intern("Brian", 1, "test@test.com", "UCLA");
+  expect(e.getRole()).toBe(testValue);
+});
+
+test("Can get school via getSchool()", () => {
+  const testValue = "UT Austin";
+  const e = new Intern("Brian", 1, "test@test.com", testValue);
+  expect(e.getSchool()).toBe(testValue);
+});
